8ac655d59cfb1c19c535d73df4006cfed2fcd1f0
ankushmeshram <ankush.meshram@gmail.com>
2013-08-27 19:21:48 +0200

    Initialising SDRE for I2S

---
+++ de.dfki.i2s.SDRE/src/com/dfki/i2s/SDRE/factories/ReasonerFactory.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.dfki.i2s.SDRE.factories;
+
+/**
+ * @author anme05
+ *
+ */
+public class ReasonerFactory {
+
+}
---
+++ de.dfki.i2s.SDRE/src/com/dfki/i2s/SDRE/interfaces/IReasoner.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.dfki.i2s.SDRE.interfaces;
+
+/**
+ * @author anme05
+ *
+ */
+public interface IReasoner {
+
+}
